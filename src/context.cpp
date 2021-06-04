a87e938cad2f33dc6666b4cdb9d919c616d3a16c
Junseok <mekangaroo01@gmail.com>
2021-06-04 19:44:14 +0900

    SSAO

--- src/context.cpp
+++ src/context.cpp
@@ -36,7 +36,23 @@ void Context::Reshape(int width, int height) {
     m_width = width;
     m_height = height;
     glViewport(0, 0, m_width, m_height);
-    m_framebuffer = Framebuffer::Create(Texture::Create(width, height, GL_RGBA));
+    m_framebuffer = Framebuffer::Create({
+        Texture::Create(width, height, GL_RGBA),
+  });
+
+    m_deferGeoFramebuffer = Framebuffer::Create({
+        Texture::Create(width, height, GL_RGBA16F, GL_FLOAT),
+        Texture::Create(width, height, GL_RGBA16F, GL_FLOAT),
+        Texture::Create(width, height, GL_RGBA, GL_UNSIGNED_BYTE),
+    });
+
+    m_ssaoFramebuffer = Framebuffer::Create({
+        Texture::Create(width, height, GL_RED),
+    });
+
+    m_ssaoBlurFramebuffer = Framebuffer::Create({
+        Texture::Create(width, height, GL_RED),
+    });
 }
 
 void Context::MouseMove(double x, double y) {
@@ -159,14 +175,64 @@ bool Context::Init() {
     m_plane->GetIndexBuffer()->Bind();
 
     m_shadowMap = ShadowMap::Create(1024, 1024);
-    m_lightingShadowProgram = Program::Create(
-        "./shader/lighting_shadow.vs", "./shader/lighting_shadow.fs");
+    m_lightingShadowProgram = Program::Create("./shader/lighting_shadow.vs", "./shader/lighting_shadow.fs");
 
     m_brickDiffuseTexture = Texture::CreateFromImage(
         Image::Load("./image/brickwall.jpg", false).get());
     m_brickNormalTexture = Texture::CreateFromImage(
         Image::Load("./image/brickwall_normal.jpg", false).get());
     m_normalProgram = Program::Create("./shader/normal.vs", "./shader/normal.fs");
+
+    m_deferGeoProgram = Program::Create("./shader/defer_geo.vs", "./shader/defer_geo.fs");
+
+    m_deferLightProgram = Program::Create("./shader/defer_light.vs", "./shader/defer_light.fs");
+    m_deferLights.resize(32);
+    for (size_t i = 0; i < m_deferLights.size(); i++) {
+        m_deferLights[i].position = glm::vec3(
+            RandomRange(-10.0f, 10.0f),
+            RandomRange(1.0f, 4.0f),
+            RandomRange(-10.0f, 10.0f));
+        m_deferLights[i].color = glm::vec3(
+            RandomRange(0.0f, i < 3 ? 1.0f : 0.0f),
+            RandomRange(0.0f, i < 3 ? 1.0f : 0.0f),
+            RandomRange(0.0f, i < 3 ? 1.0f : 0.0f));
+    }
+
+    m_ssaoProgram = Program::Create("./shader/ssao.vs", "./shader/ssao.fs");
+    m_blurProgram = Program::Create("./shader/blur_5x5.vs", "./shader/blur_5x5.fs");
+    m_model = Model::Load("./model/backpack.obj");
+
+    std::vector<glm::vec3> ssaoNoise;
+    ssaoNoise.resize(16);
+    for (size_t i = 0; i < ssaoNoise.size(); i++) {
+        // randomly selected tangent direction
+        glm::vec3 sample(RandomRange(-1.0f, 1.0f), RandomRange(-1.0f, 1.0f), 0.0f);
+        ssaoNoise[i] = sample;
+    }
+
+    m_ssaoNoiseTexture = Texture::Create(4, 4, GL_RGB16F, GL_FLOAT);
+    m_ssaoNoiseTexture->Bind();
+    m_ssaoNoiseTexture->SetFilter(GL_NEAREST, GL_NEAREST);
+    m_ssaoNoiseTexture->SetWrap(GL_REPEAT, GL_REPEAT);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 4, 4,
+        GL_RGB, GL_FLOAT, ssaoNoise.data());
+    
+    m_ssaoSamples.resize(64);
+    for (size_t i = 0; i < m_ssaoSamples.size(); i++) {
+        // uniformly randomized point in unit hemisphere
+        glm::vec3 sample(
+            RandomRange(-1.0f, 1.0f),
+            RandomRange(-1.0f, 1.0f),
+            RandomRange(0.0f, 1.0f));
+        sample = glm::normalize(sample) * RandomRange();
+
+        // scale for slightly shift to center
+        float t = (float)i / (float)m_ssaoSamples.size();
+        float t2 = t * t;
+        float scale = (1.0f - t2) * 0.1f + t2 * 1.0f;
+
+        m_ssaoSamples[i] = sample * scale;
+    }
 
     return true;
 }
@@ -198,6 +264,8 @@ void Context::Render() {
             ImGui::ColorEdit3("l.specular", glm::value_ptr(m_light.specular));
             ImGui::Checkbox("flash light", &m_flashLightMode);
             ImGui::Checkbox("l.blinn", &m_blinn);
+            ImGui::Checkbox("use ssao", &m_useSsao);
+            ImGui::DragFloat("ssao radius", &m_ssaoRadius, 0.01f, 0.0f, 5.0f);
         }
 
         ImGui::Checkbox("animation", &m_animation);
@@ -206,6 +274,49 @@ void Context::Render() {
             ImVec2(256, 256), ImVec2(0, 1), ImVec2(1, 0));
     }
     ImGui::End();
+
+    if (ImGui::Begin("G-Buffers")) {
+        const char* bufferNames[] = { "position", "normal", "albedo/specular" };
+        static int bufferSelect = 0;
+        ImGui::Combo("buffer", &bufferSelect, bufferNames, 3);
+        float width = ImGui::GetContentRegionAvailWidth();
+        float height = width * ((float)m_height / (float)m_width);
+        auto selectedAttachment =
+        m_deferGeoFramebuffer->GetColorAttachment(bufferSelect);
+        ImGui::Image((ImTextureID)selectedAttachment->Get(),
+        ImVec2(width, height), ImVec2(0, 1), ImVec2(1, 0));
+    }
+    ImGui::End();
+
+    if (ImGui::Begin("SSAO")) {
+        const char* bufferNames[] = { "original", "blurred" };
+        static int bufferSelect = 0;
+        ImGui::Combo("buffer", &bufferSelect, bufferNames, 2);
+        float width = ImGui::GetContentRegionAvailWidth();
+        float height = width * ((float)m_height / (float)m_width);
+        auto selectedAttachment =
+        bufferSelect == 0 ?
+        m_ssaoFramebuffer->GetColorAttachment() :
+        m_ssaoBlurFramebuffer->GetColorAttachment();
+        ImGui::Image((ImTextureID)selectedAttachment->Get(),
+            ImVec2(width, height), ImVec2(0, 1), ImVec2(1, 0));
+  }
+  ImGui::End();
+
+    m_cameraFront =
+        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
+        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
+        glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+
+    //m_light.position = m_cameraPos;
+    //m_light.direction = m_cameraFront;
+
+    auto projection = glm::perspective(glm::radians(45.0f),
+        (float)m_width / (float)m_height, 0.01f, 100.0f);
+    auto view = glm::lookAt(
+        m_cameraPos,
+        m_cameraPos + m_cameraFront,
+        m_cameraUp);
 
     auto lightView = glm::lookAt(m_light.position,
         m_light.position + m_light.direction,
@@ -225,29 +336,99 @@ void Context::Render() {
     m_simpleProgram->SetUniform("color", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
     DrawScene(lightView, lightProjection, m_simpleProgram.get());
 
+    m_ssaoFramebuffer->Bind();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, m_width, m_height);
+    m_ssaoProgram->Use();
+    glActiveTexture(GL_TEXTURE0);
+    m_deferGeoFramebuffer->GetColorAttachment(0)->Bind();
+    glActiveTexture(GL_TEXTURE1);
+    m_deferGeoFramebuffer->GetColorAttachment(1)->Bind();
+    glActiveTexture(GL_TEXTURE2);
+    m_ssaoNoiseTexture->Bind();
+    glActiveTexture(GL_TEXTURE0);
+    m_ssaoProgram->SetUniform("gPosition", 0);
+    m_ssaoProgram->SetUniform("gNormal", 1);
+    m_ssaoProgram->SetUniform("texNoise", 2);
+    m_ssaoProgram->SetUniform("noiseScale", glm::vec2(
+        (float)m_width / (float)m_ssaoNoiseTexture->GetWidth(),
+        (float)m_height / (float)m_ssaoNoiseTexture->GetHeight()));
+    m_ssaoProgram->SetUniform("radius", m_ssaoRadius);
+    for (size_t i = 0; i < m_ssaoSamples.size(); i++) {
+        auto sampleName = fmt::format("samples[{}]", i);
+        m_ssaoProgram->SetUniform(sampleName, m_ssaoSamples[i]);
+    }
+    m_ssaoProgram->SetUniform("transform",
+        glm::scale(glm::mat4(1.0f), glm::vec3(2.0f)));
+    m_ssaoProgram->SetUniform("view", view);
+    m_ssaoProgram->SetUniform("projection", projection);
+    m_plane->Draw(m_ssaoProgram.get());
+
+    m_ssaoBlurFramebuffer->Bind();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, m_width, m_height);
+    m_blurProgram->Use();
+    m_ssaoFramebuffer->GetColorAttachment(0)->Bind();
+    m_blurProgram->SetUniform("tex", 0);
+    m_blurProgram->SetUniform("transform",
+        glm::scale(glm::mat4(1.0f), glm::vec3(2.0f)));
+    m_plane->Draw(m_blurProgram.get());
+
+    m_deferGeoFramebuffer->Bind();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, m_width, m_height);
+    m_deferGeoProgram->Use();
+    DrawScene(view, projection, m_deferGeoProgram.get());
+
     Framebuffer::BindToDefault();
     glViewport(0, 0, m_width, m_height);
-
-    //m_framebuffer->Bind();
-
+    glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
+    m_deferLightProgram->Use();
+    glActiveTexture(GL_TEXTURE0);
+    m_deferGeoFramebuffer->GetColorAttachment(0)->Bind();
+    glActiveTexture(GL_TEXTURE1);
+    m_deferGeoFramebuffer->GetColorAttachment(1)->Bind();
+    glActiveTexture(GL_TEXTURE2);
+    m_deferGeoFramebuffer->GetColorAttachment(2)->Bind();
+    glActiveTexture(GL_TEXTURE3);
+    m_ssaoBlurFramebuffer->GetColorAttachment()->Bind();
+    glActiveTexture(GL_TEXTURE0);
+    m_deferLightProgram->SetUniform("gPosition", 0);
+    m_deferLightProgram->SetUniform("gNormal", 1);
+    m_deferLightProgram->SetUniform("gAlbedoSpec", 2);
+    m_deferLightProgram->SetUniform("ssao", 3);
+    m_deferLightProgram->SetUniform("useSsao", m_useSsao ? 1 : 0);
+    for (size_t i = 0; i < m_deferLights.size(); i++) {
+        auto posName = fmt::format("lights[{}].position", i);
+        auto colorName = fmt::format("lights[{}].color", i);
+        m_deferLightProgram->SetUniform(posName, m_deferLights[i].position);
+        m_deferLightProgram->SetUniform(colorName, m_deferLights[i].color);
+    }
+    m_deferLightProgram->SetUniform("transform",
+        glm::scale(glm::mat4(1.0f), glm::vec3(2.0f)));
+    m_plane->Draw(m_deferLightProgram.get());
 
-    m_cameraFront =
-        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
-        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
-        glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, m_deferGeoFramebuffer->Get());
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    glBlitFramebuffer(0, 0, m_width, m_height,
+        0, 0, m_width, m_height,
+        GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-    //m_light.position = m_cameraPos;
-    //m_light.direction = m_cameraFront;
+    m_simpleProgram->Use();
+    for (size_t i = 0; i < m_deferLights.size(); i++) {
+        m_simpleProgram->SetUniform("color", glm::vec4(m_deferLights[i].color, 1.0f));
+        m_simpleProgram->SetUniform("transform",
+            projection * view *
+            glm::translate(glm::mat4(1.0f), m_deferLights[i].position) *
+            glm::scale(glm::mat4(1.0f), glm::vec3(0.1f)));
+        m_box->Draw(m_simpleProgram.get());
+    }
 
-    auto projection = glm::perspective(glm::radians(45.0f),
-        (float)m_width / (float)m_height, 0.01f, 100.0f);
-    auto view = glm::lookAt(
-        m_cameraPos,
-        m_cameraPos + m_cameraFront,
-        m_cameraUp);
-
+    /*
     auto skyboxModelTransform =
         glm::translate(glm::mat4(1.0), m_cameraPos) *
         glm::scale(glm::mat4(1.0), glm::vec3(50.0f));
@@ -311,6 +492,7 @@ void Context::Render() {
     m_normalProgram->SetUniform("modelTransform", modelTransform);
     m_normalProgram->SetUniform("transform", projection * view * modelTransform);
     m_plane->Draw(m_normalProgram.get());
+    */
 
     //Framebuffer::BindToDefault();
 
@@ -368,4 +550,13 @@ void Context::DrawScene(const glm::mat4& view,
     program->SetUniform("modelTransform", modelTransform);
     m_box2Material->SetToProgram(program);
     m_box->Draw(program);
+
+    modelTransform =
+        glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.55f, 0.0f)) *
+        glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)) *
+        glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f));
+    transform = projection * view * modelTransform;
+    program->SetUniform("transform", transform);
+    program->SetUniform("modelTransform", modelTransform);
+    m_model->Draw(program);
 }
